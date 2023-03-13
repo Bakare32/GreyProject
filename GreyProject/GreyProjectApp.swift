4abf532a4c28b1260fb1313b3216b9c091d029b5
Bakare32 <bakarewariz012@gmail.com>
2023-03-13 15:31:24 +0100

    Initial Commit

---
+++ GreyProject/GreyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreyProjectApp.swift
+//  GreyProject
+//
+//  Created by USER on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GreyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
